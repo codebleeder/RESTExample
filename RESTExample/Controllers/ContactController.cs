0d062db783ead94402bdf6106c4d72b4a44caade
codebleeder <sharad.shivmath@gmail.com>
2017-01-15 22:40:35 -0500

    first commit

---
+++ RESTExample/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RESTExample.Models;
+using RESTExample.Services;
+
+namespace RESTExample.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository; 
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            contactRepository.SaveContact(contact);
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+            return response; 
+        }
+
+    }
+}
---
+++ RESTExample/Services/ContactRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RESTExample.Models;
+
+namespace RESTExample.Services
+{
+    public class ContactRepository
+    {
+        Contact[] contacts;
+        List<Contact> lContacts; 
+
+        public ContactRepository()
+        {
+            //this.contacts =  new Contact[] { new Contact { id = 1, name = "Sharad" }, new Contact { id = 2, name = "Surabhi" } };
+            lContacts = new List<Contact>();
+            lContacts.AddRange(new Contact[] { new Contact { id = 1, name = "Sharad" }, new Contact { id = 2, name = "Surabhi" } });
+        }
+        public Contact[] GetAllContacts()
+        {
+            return lContacts.ToArray<Contact>();
+        }
+        public bool SaveContact(Contact oContact)
+        {
+            try
+            {
+                lContacts.Add(oContact);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            
+        }
+    }
+}
